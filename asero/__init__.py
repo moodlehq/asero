a9e8369907002ca4b60044cfdc004c0d94461ed3
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2025-08-20 17:58:55 +0200

    Better finding ROOT_DIR up to the dir having an .env file

--- asero/__init__.py
+++ asero/__init__.py
@@ -9,7 +9,10 @@ from importlib.metadata import PackageNotFoundError, version
 from pathlib import Path
 
 # Ugly constant, but nice to have for easy testing.
-ROOT_DIR = Path(__file__).resolve().parent.parent
+ROOT_DIR = Path(__file__).resolve().parent
+# Transverse up until we find a .env file (that is required for the package to work).
+while not (ROOT_DIR / ".env").exists() and ROOT_DIR != ROOT_DIR.parent:
+    ROOT_DIR = ROOT_DIR.parent
 
 # To configure logging level globally. Note this cannot be set in config file / .env. Only via env variable.
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
